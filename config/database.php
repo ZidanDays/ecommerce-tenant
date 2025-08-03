8917ef58cf5797380fe0241a0b869f8103aa9e16
ZidanDays <zidan.dailer@gmail.com>
2025-08-03 11:28:45 +0800

    add: utils

--- config/database.php
+++ config/database.php
@@ -63,6 +63,20 @@ return [
             ]) : [],
         ],
 
+        'tenant_template' => [
+        'driver'    => 'mysql',
+        'host'      => env('DB_HOST', '127.0.0.1'),
+        'port'      => env('DB_PORT', '3306'),
+        'database'  => null,           // akan diisi otomatis oleh stancl/tenancy
+        'username'  => env('DB_USERNAME', 'root'),
+        'password'  => env('DB_PASSWORD', ''),
+        'charset'   => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix'    => '',
+        'strict'    => true,
+        'engine'    => null,
+       ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
